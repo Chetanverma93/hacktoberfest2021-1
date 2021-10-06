b40b6f89ebeff5c3ce84be2acdeab52767c97fcb
CHETAN VERMA <66515815+Chetanverma93@users.noreply.github.com>
2021-10-07 00:39:27 +0530

    Create Tree_Post_order.cpp

---
+++ C/Tree_Post_order.cpp
@@ -0,0 +1,15 @@
+//sortest way to find post order
+class Solution {
+public:
+    vector<int>result;
+    vector<int> postorderTraversal(TreeNode* root)
+    {
+        if(root==NULL)
+            return result;
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        result.push_back(root->val);
+        
+        return result;
+    }
+};
